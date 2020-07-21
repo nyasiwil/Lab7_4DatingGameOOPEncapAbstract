9bd0238bb378bdc6fe3918a4d7cebec68fcdcea1
Nyasia Williams <ny.williams123@gmail.com>
2020-07-21 10:25:41 -0400

    Add project files.

---
+++ Messages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7_4DatingGameOOPEncapAbstract
+{
+    class Messages
+    {
+        public DatingProfile sender;
+        public string messageTitle;
+        public string messageData;
+        public bool isRead;
+
+        public Messages(DatingProfile sender, string messageTitle, string messageData)
+        {
+            this.sender = sender;
+            this.messageTitle = messageTitle;
+            this.messageData = messageData;
+            isRead = false;
+        }
+
+    }
+}
